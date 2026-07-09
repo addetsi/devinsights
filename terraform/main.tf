f81a6a3f87f1689f7d39641cf79123d799323383
addetsi <godwinaddetsi12@gmail.com>
2026-07-09 15:05:25 +0200

    feat: add blob storage landing zone with unique naming

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,23 @@ resource "azurerm_resource_group" "main" {
   location = var.location
   tags     = local.common_tags
 }
+
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+resource "azurerm_storage_account" "landing" {
+  name                     = "st${var.project}${var.environment}${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags                     = local.common_tags
+}
+
+resource "azurerm_storage_container" "raw" {
+  name               = "raw"
+  storage_account_id = azurerm_storage_account.landing.id
+}
