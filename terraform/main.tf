1125534f536609a09f093de74fb8e8f8e8381b65
addetsi <godwinaddetsi12@gmail.com>
2026-07-15 09:55:24 +0200

    feat: add terraform outputs for resource names

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,3 +88,20 @@ resource "azurerm_eventhub_authorization_rule" "consumer" {
   send                = false
   manage              = false
 }
+
+
+resource "azurerm_key_vault_secret" "eventhub_producer" {
+  name         = "eventhub-producer-connection"
+  value        = azurerm_eventhub_authorization_rule.producer.primary_connection_string
+  key_vault_id = azurerm_key_vault.main.id
+
+  depends_on = [azurerm_role_assignment.kv_admin]
+}
+
+resource "azurerm_key_vault_secret" "eventhub_consumer" {
+  name         = "eventhub-consumer-connection"
+  value        = azurerm_eventhub_authorization_rule.consumer.primary_connection_string
+  key_vault_id = azurerm_key_vault.main.id
+
+  depends_on = [azurerm_role_assignment.kv_admin]
+}
